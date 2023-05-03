448bd4608420b2ff69efa93a41fc49ae21c8b026
larus-ridibundus <i.am.a.shirokuma@gmail.com>
2023-05-03 13:05:58 +0900

    upgrade

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ for creating reqif format
 
 # imageの全削除
 docker system prune -a --volumes
+
+# containerの実行
+sh run.sh
+
+# containerからデータのコピー
+docker cp <container_name>:app .
+docker cp csv2reqif-reqif-1:app .
--- app/app.py
+++ app/app.py
@@ -1,47 +1,57 @@
+# Import the required modules
 import csv
-from lxml import etree
+import xml.etree.ElementTree as ET
 
-# Define the column names in the CSV file
-csv_columns = ['ID', 'Name', 'Description']
+# Define the reqif namespace and prefix
+reqif_ns = "http://www.omg.org/spec/ReqIF/20110401/reqif.xsd"
+ET.register_namespace("reqif", reqif_ns)
+reqif_prefix = "{%s}" % reqif_ns
 
-# Define the ReqIF namespace and prefix
-reqif_uri = "http://www.omg.org/spec/ReqIF/20110401/reqif.xsd"
-reqif_prefix = "reqif"
+# Create the root element of the reqif document
+reqif_root = ET.Element(reqif_prefix + "REQ-IF")
 
-# Create the root element of the ReqIF file
-root = etree.Element("{" + reqif_uri + "}ReqIF", nsmap={reqif_prefix: reqif_uri})
+# Create the core content element and append it to the root
+core_content = ET.SubElement(reqif_root, reqif_prefix + "CORE-CONTENT")
 
-# Create the header element and add it to the root
-header = etree.Element("{" + reqif_uri + "}TheHeader", attrib={
-    "version": "1.0",
-    "comment": "ReqIF file generated from CSV data"
-})
-root.append(header)
+# Create the requirements element and append it to the core content
+requirements = ET.SubElement(core_content, reqif_prefix + "REQUIREMENTS")
 
-# Create the core content element and add it to the root
-core_content = etree.Element("{" + reqif_uri + "}CoreContent")
-root.append(core_content)
+# Create the spec objects element and append it to the requirements
+spec_objects = ET.SubElement(requirements, reqif_prefix + "SPEC-OBJECTS")
 
-# Read data from the CSV file and add it to the ReqIF file
-with open('data.csv', 'r') as csv_file:
-    print("open('data.csv', 'r')")
-    reader = csv.DictReader(csv_file, fieldnames=csv_columns)
-    for row in reader:
-        requirement = etree.Element("{" + reqif_uri + "}Requirement", attrib={
-            "IDENTIFIER": row['ID']
-        })
+# Open the csv file and read it as a dictionary
+with open("data.csv", mode="r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
 
-        name = etree.Element("{" + reqif_uri + "}Name")
-        name.text = row['Name']
-        requirement.append(name)
+    # Loop through each row in the csv file
+    for row in csv_reader:
 
-        description = etree.Element("{" + reqif_uri + "}Description")
-        description.text = row['Description']
-        requirement.append(description)
+        # Create a spec object element and append it to the spec objects
+        spec_object = ET.SubElement(spec_objects, reqif_prefix + "SPEC-OBJECT")
 
-        core_content.append(requirement)
+        # Set the identifier attribute of the spec object
+        spec_object.set("IDENTIFIER", row["ID"])
 
-# Write the ReqIF file to disk
-with open('output.reqif', 'wb') as reqif_file:
-    reqif_file.write(etree.tostring(root, pretty_print=True))
-    print(reqif_file)
+        # Set the type attribute of the spec object
+        spec_object.set("TYPE", row["SpecType"])
+
+        # Create a values element and append it to the spec object
+        values = ET.SubElement(spec_object, reqif_prefix + "VALUES")
+
+        # Create a attribute value xhtml element and append it to the values
+        attribute_value_xhtml = ET.SubElement(values, reqif_prefix + "ATTRIBUTE-VALUE-XHTML")
+
+        # Set the definition attribute of the attribute value xhtml
+        attribute_value_xhtml.set("DEFINITION", row["Type"])
+
+        # Create a value element and append it to the attribute value xhtml
+        value = ET.SubElement(attribute_value_xhtml, reqif_prefix + "THE-VALUE")
+
+        # Set the text content of the value element to the text column of the csv row
+        value.text = row["Text"]
+
+# Create an ElementTree object from the root element
+reqif_tree = ET.ElementTree(reqif_root)
+
+# Write the ElementTree object to a reqif file with pretty printing
+reqif_tree.write("example.reqif", encoding="utf-8", xml_declaration=True, method="xml")
